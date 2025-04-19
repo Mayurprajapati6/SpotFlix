4403813ba21023e9a815dc93c19394031be11746
Mayur Prajapati <mproyur685@gmail.com>
2025-04-19 15:40:52 +0530

    initial setup and added navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+import NavbarContainer from './Components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <Router>
+      <NavbarContainer />
+    </Router>
+    
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Menu.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Menu({ setIsMenuOpen }) {
+    const navigate = useNavigate();
+    return (
+        <div>
+            <p
+              onClick={() => {
+                setIsMenuOpen(false);
+                navigate('/');
+              }}
+              className='px-4 py-2 rounded-lg w-full md:w-auto cursor-pointer transition duration-200 
+                         hover:bg-slate-200 dark:hover:bg-slate-600 dark:hover:text-white
+                         bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-slate-200 flex items-center justify-center'
+            
+            > Home
+            </p>
+            
+            <p
+              onClick={() => {
+                setIsMenuOpen(false);
+                navigate('/');
+              }}
+              className='px-4 py-2 rounded-lg w-full md:w-auto cursor-pointer transition duration-200 
+                         hover:bg-slate-200 dark:hover:bg-slate-600 dark:hover:text-white
+                         bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-slate-200 flex items-center justify-center'
+            > Search
+            </p>
+
+            <p
+              onClick={() => {
+                setIsMenuOpen(false);
+                navigate('/about');
+              }}
+              className='px-4 py-2 rounded-lg w-full md:w-auto cursor-pointer transition duration-200 
+                         hover:bg-slate-200 dark:hover:bg-slate-600 dark:hover:text-white
+                         bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-slate-200 flex items-center justify-center'
+            
+            > About
+            </p>
+        </div>
+    );
+}
+
+export default Menu;
